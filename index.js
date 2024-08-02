6ace5e50b7816645a88aac6c8c2b0ea2f542cb84
Rory Goddard <goddardrory@gmail.com>
2024-08-02 15:18:56 +0200

    feat: Define BankBranch Class

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 // This one will be a little tricky. So check out this overview first: https://www.youtube.com/watch?v=sJ-c3BA-Ypo
 // 1. Create a variable to store the singleton instance of the bank branch. "bankBranchInstance"
 const bankBranchInstance = null;
-// 2. Define a class called `BankBranch` for managing branch information.
 
+// 2. Define a class called `BankBranch` for managing branch information.
+class BankBranch {
+    constructor(branchInfo) {
+        if (!bankBranchInstance) {
+            this.branchInfo = branchInfo;
+            bankBranchInstance = this
+        }
+        return bankBranchInstance
+    }
+}
 // 3. In the `BankBranch` class:
 //    - Create a constructor that takes `branchInfo` as a parameter.
 //    - Inside the constructor, check if the `bankBranchInstance` variable is null (indicating no instance exists).
